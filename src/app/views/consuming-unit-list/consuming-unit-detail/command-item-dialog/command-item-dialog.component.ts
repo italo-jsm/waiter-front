1a6aac47588b5bec301229a390e09f1866c4cf66
Italo <italojunqueira87@gmail.com>
2020-11-17 18:59:28 -0300

    changing from string to Product

--- src/app/views/consuming-unit-list/consuming-unit-detail/command-item-dialog/command-item-dialog.component.ts
+++ src/app/views/consuming-unit-list/consuming-unit-detail/command-item-dialog/command-item-dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { Product } from 'src/app/shared/model/product.model';
+
+import { ProductService } from 'src/app/shared/service/product.service';
 
 @Component({
   selector: 'app-command-item-dialog',
@@ -10,16 +15,34 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class CommandItemDialogComponent implements OnInit {
 
   constructor(
+    private productService: ProductService,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<CommandItemDialogComponent>
     ) { }
   public itemForm: FormGroup;
+
+  products: Product[]
+
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
   ngOnInit(): void {
     this.itemForm = this.fb.group({
       product: ['', [Validators.required]],
       quantity: ['', [Validators.required]]
     });
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
   createItem(){
@@ -32,6 +55,10 @@ export class CommandItemDialogComponent implements OnInit {
   cancel(){
     this.dialogRef.close();
     this.itemForm.reset();
+  }
+
+  getAllProducts(){
+    this.productService.getProducts().subscribe(data => this.products = data)
   }
 
 }
